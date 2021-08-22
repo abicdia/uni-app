0998ba79084b555c17e02351c7ec93b81b94f127
xiaowen <237406984@qq.com>
2021-08-22 18:48:51 +0800

    集中管理api

--- shop/main.js
+++ shop/main.js
@@ -7,9 +7,17 @@ App.mpType = 'app'
 
 // 引入全局uView
 import uView from 'uview-ui'
+// 这里需要写在最后，是为了等Vue创建对象完成，引入"app"对象(也即页面的"this"实例)
 Vue.use(uView);
 
 const app = new Vue({
     ...App
 })
+// http拦截器，将此部分放在new Vue()和app.$mount()之间，才能App.vue中正常使用
+import httpInterceptor from '@/common/http.interceptor.js'
+Vue.use(httpInterceptor, app)
+
+// http接口API集中管理引入部分
+import httpApi from '@/common/http.api.js'
+Vue.use(httpApi, app)
 app.$mount()
--- shop/pages/index/index.vue
+++ shop/pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-	首页
+		首页
 	</view>
 </template>
 
@@ -11,8 +11,29 @@
 				title: 'Hello'
 			}
 		},
-		onLoad() {
+		async onLoad() {
+			// await this.$u.get('/api/index').then(res => {
+			// 	console.log(res)
+			// })
+			// console.log('sss');
+			// const res =await this.$u.get('/api/index')
+			// console.log(res);
+			// console.log("111");
 
+			// const res1=await this.$u.patch('/api/orders/1/confirm',{name:'tom'})
+			// console.log(res1,'op');
+
+			//集中管理api
+			// const res =await this.$u.api.index()
+			// console.log(res)
+
+			//测试登录
+			const data = {
+				email: 'test@a.com',
+				password: '123123'
+			}
+			const res=await this.$u.api.authLogin(data)
+			console.log(res)
 		},
 		methods: {
 
